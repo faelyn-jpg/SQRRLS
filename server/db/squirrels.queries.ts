ad1d9218c591ac49ed35ea618bd3dbd9788f164f
Abdirazak Jamae <abdirasak78@icloud.com>
2024-11-08 11:08:39 +1300

    routes

--- server/db/squirrels.queries.ts
+++ server/db/squirrels.queries.ts
@@ -6,4 +6,8 @@ import { Squirrel } from '../../models/squirrel.ts'
 export async function getAllSquirrels(db = connection): Promise<Squirrel[]> {
   return await db('squirrels').select('*')
 }
+
+export function getSquirrelById(id: any) {
+  throw new Error('Function not implemented.')
+}
 //MF! why!! ^^Type 'QueryBuilder' is missing the following properties from type 'Squirrel[]': length, pop, push, concat, and 28 more.ts(2740)
