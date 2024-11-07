e57d4f0aabb043befec5848070a15b8c40a3933c
faelyn-jpg <169906179+faelyn-jpg@users.noreply.github.com>
2024-11-07 16:26:20 +1300

    build routing

---
+++ client/components/Home.tsx
@@ -0,0 +1,16 @@
+// import { useFruits } from '../hooks/useFruits.ts'
+
+function Home() {
+  // const { data } = useFruits()
+
+  return (
+    <>
+      <div className="home">
+        <h1>!</h1>
+        {/* <ul>{data && data.map((fruit) => <li key={fruit}>{fruit}</li>)}</ul> */}
+      </div>
+    </>
+  )
+}
+
+export default Home
