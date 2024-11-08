2d0b0abaa09efc27e5b87e7e2436a1e228066b69
Zak Hynd <zakhynd@gmail.com>
2024-11-08 19:13:29 +1300

    add homepage link

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -1,11 +1,11 @@
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 
 function Layout() {
   return (
     <>
       <div className="container">
         <div className="title">
-          <h1>SQRRL!</h1>
+          <Link to = {'/'}><h1>SQRRL!</h1></Link>
         </div>
         {/* This 'main' div is only for styling (so we can use flexbox) */}
         <div className="main">
